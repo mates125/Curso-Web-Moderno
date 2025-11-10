33c7207dfc3ba7c78e9c47fdb6ea8fdc68f9e88a
Mateus Lopes <mtslopes.dev@gmail.com>
2025-11-10 16:28:32 -0300

    asdfg

--- exercicios_web/html/exercicios/form/server.js
+++ exercicios_web/html/exercicios/form/server.js
@@ -4,8 +4,9 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({ extended: true}))
 
-app.post('/usuarios', (req, resp) => {
+app.get('/usuarios', (req, resp) => {
     console.log(req.body)
+    console.log(req.query)
     resp.send('<h1>Parabéns. Usuário Incluído!</h1>')
 })
 
